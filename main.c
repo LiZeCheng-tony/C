f993727fdff9a0b4dadbf3b4e1491485f6fc0c28
LiZeCheng-tony <L2763404373@hotmail.com>
2020-03-14 13:47:09 +0800

    master

--- main.c
+++ main.c
@@ -1,8 +1,28 @@
 #include<stdio.h>
+#define num 59
+#define MAX_LINE 1000000
+int student_chinese[num];
+int student_maths[num];
+int student_english[num];
+int student_name[MAX_LINE];
+int name()
+{
+ FILE *fp;            /*文件指针*/
+ int len;             /*行字符个数*/
+ if((fp = fopen("name.txt","r")) == NULL)
+ {
+ perror("fail to read");
+ exit (1) ;
+ }
+ while(fgets(student_name,MAX_LINE,fp) != NULL)
+ {
+ printf("%d \n",student_name);
+ }
+}
 int main()
 {
     int a;
-    system("cls");
+    name();
     printf("+----------------------+\n");
     printf("+   1.chinese          +\n");
     printf("+   2.maths            +\n");
