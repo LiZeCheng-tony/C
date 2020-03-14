7a781e3c0b29c510ae6455de4d12e03ca96de476
LiZeCheng-tony <L2763404373@hotmail.com>
2020-03-14 11:47:21 +0800

    master

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
 #include<stdio.h>
+#include <stdlib.h>
 int main()
 {
-    printf("HelloWord!");
-    return 0;
+    int a;
+    system("cls");
+    printf("+----------------------+\n");
+    printf("+   1.chinese          +\n");
+    printf("+   2.maths            +\n");
+    printf("+   3.english          +\n");
+    printf("+----------------------+\n");
+    printf(":")
+    scanf("%d",&a);
+    if (a==1)
+    {
+      chinese();
+    }else if(a==2){
+        maths();
+    }else if(a==3){
+        english();
+    }else{
+        printf("sorry,enter angin!");
+        system("pause");
+        main();
+    }
+}
+int chinese()
+{
+
+}
+int maths()
+{
+
+}
+int english()
+{
+
 }
