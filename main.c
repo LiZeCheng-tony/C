ca6af34f419cbe5d154416f8537a62b89ce25779
LiZeCheng-tony <L2763404373@hotmail.com>
2020-03-21 22:15:26 +0800

    master

--- main.c
+++ main.c
@@ -1,100 +1,24 @@
 #include<stdio.h>
-#define num 3
-int student_chinese[num];
-int student_maths[num];
-int student_english[num];
-int student_name[num];
-
-int name()
-{
+int a;                                                   //人数
+char student_name[];                                     //人名
+int b;                                                   //科目数
+int name(){
     FILE *fp;
-    int len;
-    int a=1;
-    char ch;
-    if((fp=fopen("name.txt","r+"))==NULL)
+    int flag,line=0,count = 0;
+    if((fp=fopen("name.txt","r"))==NULL)
     {
-        printf("sorry,can't read name.txt!\n");
-    }
-    while((ch=fgetc(fp))!=EOF)
-        if(ch==' ' || ch=='\t' || ch=='\n')
-        {
-            len++;
-            a++;
-        }
-        else
-        {
-            student_name[a]=ch;
-        }
-    for (int i = 1; i <=num; i++)
-    {
-        printf("%s\n",student_name[a]);
-    }
-}
-int main()
-{
-    int a;
-    name();
-    printf("+----------------------+\n");
-    printf("+   1.chinese          +\n");
-    printf("+   2.maths            +\n");
-    printf("+   3.english          +\n");
-    printf("+   4.read             +\n");
-    printf("+----------------------+\n");
-    printf(":");
-    scanf("%d",&a);
-    if (a==1)
-    {
-        chinese();
-    }else if(a==2){
-        maths();
-    }else if(a==3){
-        english();
-    }else if(a==4){
-        read();
-    }else{
-        printf("sorry,enter angin!\n");
+        printf("can't find/open name.txt!\n");
         system("pause");
-        main();
     }
+    while(!feof(fp))
+    {
+    flag = fgetc(fp);
+    if(flag == '\n')
+      count++;
+    }
+    file_row = count + 1;                               //加上最后一行
 }
-int chinese()
-{
-    int a,b;
+int main(){
     system("cls");
-    FILE * fp;
-    if(fp=fopen("chinese.txt","a")==NULL)
-    {
-        printf("sorry,don't find chinese.txt\n");
-    }
-    printf("----------Chinese----------\n");
-    for(int i=1;i<=num;i++)
-    {
-        printf("%s:",student_name);
-        scanf("%d",&student_chinese[i]);
-        printf("\n");
-        if(student_chinese[i]==' ')
-        {
-            student_chinese[i]=0;
-        }
-    }
-    printf("-----------ENTER END------------");
-    fputs(student_chinese,fp);
-        fputs("\n",fp);
-        fclose(fp);
-        printf("-----------SAVE OK--------------");
-        system("pause");
-        system("cls");
-        main();
-}
-int maths()
-{
-
-}
-int english()
-{
-
-}
-int read()
-{
-
+    name();
 }
