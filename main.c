983ce0e3191a65c42b929fcd14e75d1accfc14bf
LiZeCheng-tony <L2763404373@hotmail.com>
2020-03-23 17:27:33 +0800

    master

--- main.c
+++ main.c
@@ -2,13 +2,24 @@
 int a;                                                   //人数
 char student_name[];                                     //人名
 int b;                                                   //科目数
+int z;                                                   //输入（write）函数
 int name(){
     FILE *fp;
     int flag,line=0,count = 0;
+    char c;                                              //YES/NO
     if((fp=fopen("name.txt","r"))==NULL)
     {
-        printf("can't find/open name.txt!\n");
-        system("pause");
+        printf("无法打开\"naem.txt\"\n");
+        printf("是否创建\"name.txt?\"\n");
+        printf("Y/n:");
+        scanf("%c",&c);
+        if(c=='Y'|c=='y'|c=="\n"|c=="yes"){
+            fp=fopen("name.txt","w");
+            int z=1;
+            write();
+        }else{
+            exit(1);
+        }
     }
     while(!feof(fp))
     {
@@ -16,7 +27,11 @@ int name(){
     if(flag == '\n')
       count++;
     }
-    file_row = count + 1;                               //加上最后一行
+    line = count + 1;                                   //加上最后一行
+    printf("已读取配置文件%d行",line);
+}
+int write(){
+    
 }
 int main(){
     system("cls");
